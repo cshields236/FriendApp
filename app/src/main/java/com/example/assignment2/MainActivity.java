b5317f16295ff72954a617edf785290963e1f8f9
Conor Shields <42182485+cshields236@users.noreply.github.com>
2019-11-10 13:59:02 +0000

    fixed validations on add

--- app/src/main/java/com/example/assignment2/MainActivity.java
+++ app/src/main/java/com/example/assignment2/MainActivity.java
@@ -39,52 +39,29 @@ public class MainActivity extends AppCompatActivity {
         String username = un.getText().toString();
         String email = em.getText().toString();
         String phoneNum = pn.getText().toString();
-        boolean isIns = false;
 
-        Cursor res = myDB.getAllData();
-        if (res.getCount() == 0) {
-            //show message
 
-            return;
-        } else {
-            StringBuffer buffer = new StringBuffer();
-            while (res.moveToNext()) {
-//                buffer.append("ID:" + res.getString(0)+ "\n");
-//                buffer.append("USERNAME:" + res.getString(1)+ "\n");
-//                buffer.append("EMAIL:" + res.getString(2)+ "\n");
-//                buffer.append("PHONENUM:" + res.getString(3)+ "\n \n");
-                //showMessage("SUCCESS", buffer.toString());
-                Friend friend = new Friend(res.getString(1), res.getString(2), Integer.parseInt(res.getString(3)));
-                friends.add(friend);
-            }
+        if (username.isEmpty() || email.isEmpty() || phoneNum.isEmpty()) {
+
+            Toast.makeText(this, "Data Not Inserted, Please enter all details  ", Toast.LENGTH_LONG).show();
+
+        } else if (username.contains(" ") || email.contains(" ")) {
+            Toast.makeText(this, "Data Not Inserted, username or email must not contain spaces", Toast.LENGTH_LONG).show();
+
+        } else if(!email.matches("^(.+)@(.+)")){
+            Toast.makeText(this, "Data Not Inserted, email must be formatted correctly", Toast.LENGTH_LONG).show();
+            em.setError("Email must be formatted correctly");
         }
-
-
-            if (username.isEmpty() || email.isEmpty() || phoneNum.isEmpty()) {
-                Toast.makeText(this, "Please Enter all Details", Toast.LENGTH_LONG).show();
-                isIns = false;
-            } else if (username.contains(" ") || email.contains(" ")) {
-                Toast.makeText(this, "Please Enter all Details", Toast.LENGTH_LONG).show();
-                isIns = false;
-//            } else if (f.getUsername().equalsIgnoreCase(username) || f.getEmail().equalsIgnoreCase(email) || f.getPhoneNum() == Integer.parseInt(phoneNum)) {
-//                Toast.makeText(this, "User already in database", Toast.LENGTH_LONG).show();
-//                isIns = false;
-//            } else {
-                friend = new Friend(username, email, Integer.parseInt(phoneNum));
-                friends.add(friend);
-
-                isIns = myDB.insertData(username, email, Integer.parseInt(phoneNum));
-            }
-
-        if (isIns = true) {
-            Toast.makeText(this, "Friend Added ", Toast.LENGTH_LONG).show();
+        else {
+            friend = new Friend(username, email, Integer.parseInt(phoneNum));
+            friends.add(friend);
+            myDB.insertData(username, email, Integer.parseInt(phoneNum));
+            Toast.makeText(this, username +" Added to your Friends List!", Toast.LENGTH_LONG).show();
             un.setText("");
             em.setText("");
             pn.setText("");
-
-        } else {
-            Toast.makeText(this, "Data Not Inserted ", Toast.LENGTH_LONG).show();
         }
+
 
 
     }
